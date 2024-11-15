f34052ba3e5e635424401991afede60f424d70ae
Areena-Dilawar <areenadilawar@gmail.com>
2024-11-15 22:47:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express();
 const { Schema } = mongoose;
 
 mongoose.connect('mongodb://localhost:27017/Books')
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("Connection error:", err));
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error("Connection error:", err));
 
-  const BookSchema = new Schema({
+const BookSchema = new Schema({
     title: { type: String, required: true },
     author: { type: String, required: true },
     year: { type: Number },
@@ -29,12 +29,25 @@ app.get('/books/:id', async (req, res) => {
     const object = await Book.findById(id);
     res.json({ "message": "object found", object });
 });
-
-app.put('book/:id', async (req, res)=>{
+app.put('update/:id', async (req, res) => {
     const id = req.params.id
     const object = req.body
-    const updatedBook =  await Book.findByIdAndUpdate(id, object);
-})
+    const updatedBook = await Book.findByIdAndUpdate(id, object, { new: true });
+    if (updatedBook === null) {
+        return res.status(404).send("Book not found");
+    }
+    res.status(200).send(updatedBook);
+});
+
+app.delete('delete/:id', async (req, res) => {
+    const id = req.params.id
+    const DeletedBook = await Book.findByIdAndDelete(id);
+    if (DeletedBook === null) {
+        return res.status(404).send("Book not found");
+    }
+    res.status(200).send(DeletedBook);
+});
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
